fd95df3709d8fe29cf42119a84a3a179470f3b81
Purushoth132004 <mpurushoth475@gmail.com>
2026-02-17 21:43:10 +0530

    Update connect.js

--- Learn/llm/connect.js
+++ Learn/llm/connect.js
@@ -1,6 +1,6 @@
 import { HfInference } from "@huggingface/inference";
 
-const hf = new HfInference();
+//const hf = new HfInference();
 
 async function run(text){
   const result = await hf.chatCompletion({
